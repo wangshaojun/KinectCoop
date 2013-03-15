17d86cc3a0a208155969d330774b4896793c8f73
Ian-Huang <dk781020@hotmail.com>
2013-03-15 15:43:47 +0800

    Stage2 加入骨架追蹤判斷 .

--- Assets/Scripts/Commons/DetectSkeletonisEnable.cs
+++ Assets/Scripts/Commons/DetectSkeletonisEnable.cs
@@ -1,31 +1,34 @@
 using UnityEngine;
 using System.Collections;
 
-public class DetectSkeletonisEnable : MonoBehaviour {
-
+public class DetectSkeletonisEnable : MonoBehaviour
+{
     public SkeletonWrapper skeletonWrapper;
-    private bool _skeletonIsEnable = false;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public bool SkeletonIsEnable = false;
 
-        
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //如果抓不到骨架
-        if (skeletonWrapper.trackedPlayers[0] == -1)
-            _skeletonIsEnable = false;
+        if (this.skeletonWrapper.trackedPlayers[0] == -1)
+        {
+            this.SkeletonIsEnable = false;
+        }
         else
-            _skeletonIsEnable = true;
-        
+        {
+            this.SkeletonIsEnable = true;
+        }
 
-        
-        if (_skeletonIsEnable)
+
+        if (SkeletonIsEnable)
             this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
         else
             this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-        
-	}
+    }
 }
