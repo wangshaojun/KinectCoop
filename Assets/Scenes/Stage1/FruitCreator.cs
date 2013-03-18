7f8999b1efd2fe48d4f3d5b0f77596e8c80ab4d8
cherlyn liu <cl_liu@hotmail.com>
2013-03-18 17:56:46 +0800

    stage1 修改顏色方向、加入wasd代替swipe

--- Assets/Scenes/Stage1/FruitCreator.cs
+++ Assets/Scenes/Stage1/FruitCreator.cs
@@ -1,52 +1,60 @@
 using UnityEngine;
 using System.Collections;
 
-public class FruitCreator : MonoBehaviour {
+public class FruitCreator : MonoBehaviour
+{
     public static int ikind = 0;
+    public static bool isMoving = false;
     public static Transform f1, f2, f3, f4;
     public Transform[] fruits = new Transform[4] { f1, f2, f3, f4 };
     public Transform dir1, dir2, dir3, dir4;
+    Vector3 origin_pos = new Vector3(0, 0, 0);
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         AddFruit();
+        origin_pos = fruits[ikind].position;
         //call fruitElement update function
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (LevelController_1.isBingo)  //配對成功，水果往對應的方向移動、消失
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)  //水果往對應的方向移動、消失
         {
-            Debug.Log("執行水果配對");
-            fruits[ikind].position += new Vector3(-10 * Time.deltaTime, 0, 0); //red
+            Debug.Log(fruits[ikind].position);
+
+            fruits[ikind].position += new Vector3(-10 * Time.deltaTime, 0, 0);
             if (fruits[ikind].position.x < -20)
             {
                 DeleteFruit();
-                fruits[ikind].position = new Vector3(0, 0, 55); //還原位置
-                LevelController_1.isBingo = false;
+                fruits[ikind].position = origin_pos; //還原位置
+                isMoving = false;
                 AddFruit(); //置換水果
             }
         }
-	}
+    }
+
     void AddFruit()
     {
         ikind = Random.Range(0, 4);
         //ikind = 0;  //test
         Instantiate(fruits[ikind]);
-        
+
         switch (ikind)
         {
             case 0: //left, green
-                Instantiate(dir3);
+                Instantiate(dir1);
                 print("green");
                 break;
-            case 1: //up, yellow
+            case 1: //up, red
                 Instantiate(dir2);
-                print("yellow");
-                break;
-            case 2: //down, red
-                Instantiate(dir1);
                 print("red");
+                break;
+            case 2: //down, yellow
+                Instantiate(dir3);
+                print("yellow");
                 break;
             case 3: //right, blue
                 Instantiate(dir4);
@@ -57,7 +65,8 @@ public class FruitCreator : MonoBehaviour {
                 break;
         }
     }
-    void DeleteFruit() {
+    void DeleteFruit()
+    {
         Destroy(fruits[ikind]);
         switch (ikind)
         {
--- Assets/Scenes/Stage1/LevelController_1.cs
+++ Assets/Scenes/Stage1/LevelController_1.cs
@@ -4,18 +4,22 @@ using System.Collections;
 public class LevelController_1 : MonoBehaviour
 {
     public static bool isBingo = false;
-    Transform correctPlane;
-	//此關卡揮手動作判斷為:往右需"左手往右揮"、往左需"右手往左揮"，其他動作尚未設定
-    
+    public Transform correctPlane;
+    //此關卡揮手動作判斷為:往右需"左手往右揮"、往左需"右手往左揮"，其他動作尚未設定
+
+
     // Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //判斷手揮動方向是否和顏色方位吻合
-        if (Gesture.isSwipeLeft){//left, red    
+        if (Gesture.isSwipeLeft)
+        {//left, red    
             if (FruitCreator.ikind == 0) isBingo = true;
             Gesture.isSwipeLeft = false;
         }
@@ -36,14 +40,36 @@ public class LevelController_1 : MonoBehaviour
             Gesture.isSwipeRight = false;
         }*/
 
-        if (isBingo) {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            if (FruitCreator.ikind == 2) isBingo = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            if (FruitCreator.ikind == 0) isBingo = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            if (FruitCreator.ikind == 1) isBingo = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            if (FruitCreator.ikind == 3) isBingo = true;
+        }
+
+        if (isBingo)
+        {
             //成功反應
             Instantiate(correctPlane);//出現幾秒後刪除，還沒寫
 
             //呼叫置換水果的函式，還沒寫
-
+            Debug.Log("bingo!");
+            FruitCreator.isMoving = true;
             isBingo = false;
         }
 
-	}
+    }
 }
