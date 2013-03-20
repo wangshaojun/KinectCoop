c01ec97a4be27dc72b944624d458dc4a1d45e00b
KinectTest <dk781020@hotmail.com>
2013-03-20 18:37:47 +0800

    Stage3 手臂偵測

--- Assets/Scenes/Stage3/S3Scripts/DetectArm.cs
+++ Assets/Scenes/Stage3/S3Scripts/DetectArm.cs
@@ -1,27 +1,36 @@
 using UnityEngine;
 using System.Collections;
 
-public class DetectArm : MonoBehaviour {
+public class DetectArm : MonoBehaviour
+{
 
     public GameObject DAGController;
     private SkeletonInformation DASkelForm;
+
+    public DetectSkeletonisEnable detectSkeleton_script;
     // Use this for initialization
     void Start()
     {
         DASkelForm = DAGController.GetComponent<SkeletonInformation>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (DASkelForm.HandLeftPos.y < DASkelForm.ShoulderLeftPos.y || DASkelForm.HandRightPos.y < DASkelForm.ShoulderRightPos.y)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.detectSkeleton_script.SkeletonIsEnable)
         {
-            Time.timeScale = 0.00000001f;
-           // this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            if (DASkelForm.HandLeftPos.y < DASkelForm.ShoulderLeftPos.y || DASkelForm.HandRightPos.y < DASkelForm.ShoulderRightPos.y)
+            {
+                this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+                Time.timeScale = 0.00000001f;
+                
+            }
+            else
+            {
+                this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+                Time.timeScale = 1;
+                
+            }
         }
-        else
-        {
-            Time.timeScale = 1;
-           // this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        }
-	}
+    }
 }
