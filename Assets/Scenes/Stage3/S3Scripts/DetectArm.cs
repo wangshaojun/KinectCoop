a283c4efc6195b6fe67d12f121b513ebe104f210
Charles-Wang <charles85014@livemail.tw>
2013-03-21 17:42:09 +0800

    HandGlideModel

--- Assets/Scenes/Stage3/S3Scripts/DetectArm.cs
+++ Assets/Scenes/Stage3/S3Scripts/DetectArm.cs
@@ -32,5 +32,7 @@ public class DetectArm : MonoBehaviour
                 
             }
         }
+        else
+            this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
     }
 }
