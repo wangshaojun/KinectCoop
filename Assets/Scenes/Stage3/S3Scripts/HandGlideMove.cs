c4d29bd32a44c1fa30ad0cdce9a786c9f57ebab4
KinectTest <dk781020@hotmail.com>
2013-03-21 18:18:38 +0800

    Stage3 model complete

--- Assets/Scenes/Stage3/S3Scripts/HandGlideMove.cs
+++ Assets/Scenes/Stage3/S3Scripts/HandGlideMove.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
 using System.Collections;
 
-public class HandGlideMove : MonoBehaviour {
+public class HandGlideMove : MonoBehaviour
+{
     public GameObject HGMGController;
     private SkeletonInformation HGMSkelForm;
     public float HandGlideRotateY;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         HGMSkelForm = HGMGController.GetComponent<SkeletonInformation>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         this.transform.Translate(0, -1 * Time.deltaTime, 3 * Time.deltaTime);
-		if(HGMSkelForm.HandRightPos.y > HGMSkelForm.ShoulderRightPos.y && 
-			HGMSkelForm.HandLeftPos.y > HGMSkelForm.ShoulderLeftPos.y)
-            this.transform.Rotate(0, -(HGMSkelForm.HandRightPos.y - HGMSkelForm.HandLeftPos.y) * this.HandGlideRotateY, 0);
-      /*  if (Input.GetKey("left") ) {
-            this.transform.Rotate(0, -15 * Time.deltaTime,0);
-        }
-        if (Input.GetKey("right"))
-        {
-            this.transform.Rotate(0, 15 * Time.deltaTime, 0);
-        }*/
-	}
+        if (HGMSkelForm.HandRightPos.y > HGMSkelForm.ShoulderRightPos.y &&
+            HGMSkelForm.HandLeftPos.y > HGMSkelForm.ShoulderLeftPos.y)
+            this.transform.Rotate(0, -(HGMSkelForm.HandRightPos.y - HGMSkelForm.HandLeftPos.y) * this.HandGlideRotateY * Time.deltaTime, 0);
+        /*  if (Input.GetKey("left") ) {
+              this.transform.Rotate(0, -15 * Time.deltaTime,0);
+          }
+          if (Input.GetKey("right"))
+          {
+              this.transform.Rotate(0, 15 * Time.deltaTime, 0);
+          }*/
+    }
 }
