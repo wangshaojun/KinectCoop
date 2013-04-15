0f21aff45dd1204dd1c569e8cb0e4b13948abf91
KinectTest <dk781020@hotmail.com>
2013-04-15 18:02:15 +0800

    Stage2 修改角色控制

--- Assets/Scenes/Stage2/Script/MoveController_Stage2.cs
+++ Assets/Scenes/Stage2/Script/MoveController_Stage2.cs
@@ -12,61 +12,77 @@ public class MoveController_Stage2 : MonoBehaviour
     public float Knne_Hip_Delta = 0.25f;                        //膝蓋與屁股間的差距(質越小，腿需抬越高)
     public float AddSpeed = 5;                                  //每一步增加的速度
 
-    public enum FeetRaiseState
+    public enum FeetState
     {
-        None, Right, Left
+        RightRaise, RightDown, LeftRaise, LeftDown
     }
-    private FeetRaiseState FeetState { get; set; }
+    private FeetState currentfeetState { get; set; }
 
     // Use this for initialization
     void Start()
     {
         this.skeletonInfo_script = this.skeletonInfoObject.GetComponent<SkeletonInformation>();
-        this.FeetState = FeetRaiseState.None;
+
+        this.currentfeetState = FeetState.LeftDown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //print("HipLeftPos"+this.skeletonInfo_script.HipLeftPos.y);
-        //print("KneeLeftPos" + this.skeletonInfo_script.KneeLeftPos.y);
-        //print(this.rigidbody.velocity);
         if (this.SkeletonIsEnableScript.SkeletonIsEnable)
         {
-            ////確認手是否平舉
-            //if (Mathf.Abs(this.skeletonInfo_script.HandLeftPos.y - this.skeletonInfo_script.ShoulderLeftPos.y) < 0.1f &&
-            //    Mathf.Abs(this.skeletonInfo_script.HandRightPos.y - this.skeletonInfo_script.ShoulderRightPos.y) < 0.1f)
-            //{
-            //    //(未完成) Kinect偵測玩家的處理
-            //    //-----------------------------
-            //}
             if (this.skeletonInfo_script.HipCenterPos.y > 1)
             {
-                if (this.FeetState != FeetRaiseState.Left)
+                switch (this.currentfeetState)
                 {
-                    //確認腳的動作
-                    if (this.skeletonInfo_script.HipLeftPos.y - this.skeletonInfo_script.KneeLeftPos.y < this.Knne_Hip_Delta)
-                    {
-                        print("Left");
-                        this.FeetState = FeetRaiseState.Left;
-                        this.transform.rigidbody.velocity += this.transform.TransformDirection(new Vector3(0, 0, this.AddSpeed));
-                    }
-                }
-                else if (this.FeetState != FeetRaiseState.Right)
-                {
-                    //確認腳的動作
-                    if (this.skeletonInfo_script.HipRightPos.y - this.skeletonInfo_script.KneeRightPos.y < this.Knne_Hip_Delta)
-                    {
-                        print("Right");
-                        this.FeetState = FeetRaiseState.Right;
-                        this.transform.rigidbody.velocity += this.transform.TransformDirection(new Vector3(0, 0, this.AddSpeed));
-                    }
+                    case FeetState.RightRaise:
+                        print(Mathf.Abs(this.skeletonInfo_script.KneeLeftPos.y - this.skeletonInfo_script.KneeRightPos.y).ToString() + "," + Mathf.Abs(this.skeletonInfo_script.KneeLeftPos.z - this.skeletonInfo_script.KneeRightPos.z).ToString());
+                        if (Mathf.Abs(this.skeletonInfo_script.KneeLeftPos.y - this.skeletonInfo_script.KneeRightPos.y) < 0.05f)
+                        {
+                            if (Mathf.Abs(this.skeletonInfo_script.KneeLeftPos.z - this.skeletonInfo_script.KneeRightPos.z) < 0.05f)
+                            {
+                                print("RightDown");
+                                this.currentfeetState = FeetState.RightDown;
+                                this.transform.rigidbody.velocity += this.transform.TransformDirection(new Vector3(0, 0, this.AddSpeed));
+                            }
+                        }
+                        break;
+
+                    case FeetState.RightDown:
+                        if (this.skeletonInfo_script.HipLeftPos.y - this.skeletonInfo_script.KneeLeftPos.y < this.Knne_Hip_Delta)
+                        {
+                            print("LeftRaise");
+                            this.currentfeetState = FeetState.LeftRaise;
+                            //this.transform.rigidbody.velocity += this.transform.TransformDirection(new Vector3(0, 0, this.AddSpeed));
+                        }
+                        break;
+
+                    case FeetState.LeftRaise:
+                        print(Mathf.Abs(this.skeletonInfo_script.KneeLeftPos.y - this.skeletonInfo_script.KneeRightPos.y).ToString() + "," + Mathf.Abs(this.skeletonInfo_script.KneeLeftPos.z - this.skeletonInfo_script.KneeRightPos.z).ToString());
+
+                        if (Mathf.Abs(this.skeletonInfo_script.KneeLeftPos.y - this.skeletonInfo_script.KneeRightPos.y) < 0.05f)
+                        {
+                            if (Mathf.Abs(this.skeletonInfo_script.KneeLeftPos.z - this.skeletonInfo_script.KneeRightPos.z) < 0.05f)
+                            {
+                                print("LeftDown");
+                                this.currentfeetState = FeetState.LeftDown;
+                                this.transform.rigidbody.velocity += this.transform.TransformDirection(new Vector3(0, 0, this.AddSpeed));
+                            }
+                        }
+                        break;
+
+                    case FeetState.LeftDown:
+                        if (this.skeletonInfo_script.HipRightPos.y - this.skeletonInfo_script.KneeRightPos.y < this.Knne_Hip_Delta)
+                        {
+                            print("RightRaise");
+                            this.currentfeetState = FeetState.RightRaise;
+                            //this.transform.rigidbody.velocity += this.transform.TransformDirection(new Vector3(0, 0, this.AddSpeed));
+                        }
+                        break;
                 }
             }
             if (Input.GetKeyUp(KeyCode.A))
             {
-                //this.transform.rigidbody.AddForce(new Vector3(0, 0, 10));
-                //this.transform.Translate(new Vector3(0, 0, 1));
                 this.transform.rigidbody.velocity += this.transform.TransformDirection(new Vector3(0, 0, this.AddSpeed));
             }
         }
